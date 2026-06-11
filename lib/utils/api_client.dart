8573e0c6e4f741b6c7ac03374993ddad5df92a55
akufarish <risfarishk@gmail.com>
2026-06-11 11:16:46 +0800

    add dio pretty logger

--- lib/utils/api_client.dart
+++ lib/utils/api_client.dart
@@ -1,6 +1,7 @@
 import 'package:admin_pegawai/utils/token_manager.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 class ApiClient {
   ApiClient._internal();
@@ -42,6 +43,17 @@ class ApiClient {
           }
           return handler.next(e);
         },
+      ),
+    );
+
+    dioInstance.interceptors.add(
+      PrettyDioLogger(
+        requestHeader: true,
+        requestBody: true,
+        responseBody: true,
+        responseHeader: false,
+        error: true,
+        compact: true,
       ),
     );
 
