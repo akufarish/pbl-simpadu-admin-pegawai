ca25aa7d5ba430a3696fd79414566f9695fb9bd1
akufarish <risfarishk@gmail.com>
2026-06-14 17:48:51 +0800

    verifikasi navigasi ke detail

--- lib/features/dashboard/presentation/screen/dashboard_screen.dart
+++ lib/features/dashboard/presentation/screen/dashboard_screen.dart
@@ -3,8 +3,10 @@ import 'package:admin_pegawai_bloc/core/components/verifikasi_card.dart';
 import 'package:admin_pegawai_bloc/features/auth/presentation/cubit/auth_cubit.dart';
 import 'package:admin_pegawai_bloc/features/pegawai/presentation/cubit/pegawai_cubit.dart';
 import 'package:admin_pegawai_bloc/features/presensi/presentation/cubit/presensi_cubit.dart';
+import 'package:admin_pegawai_bloc/features/verifikasi/domain/entities/verifikasi_entity.dart';
 import 'package:admin_pegawai_bloc/features/verifikasi/presentation/cubit/verifikasi_count_cubit.dart';
 import 'package:admin_pegawai_bloc/features/verifikasi/presentation/cubit/verifikasi_cubit.dart';
+import 'package:admin_pegawai_bloc/features/verifikasi/presentation/screen/detail_verifikasi_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:skeletonizer/skeletonizer.dart';
@@ -226,14 +228,26 @@ class VerifikasiWidget extends StatelessWidget {
             padding: EdgeInsets.fromLTRB(23, 22, 23, 20),
             sliver: SliverList(
               delegate: SliverChildBuilderDelegate((context, index) {
-                final dosen = state.dataVerifikasi[index];
+                final verifikasi = state.dataVerifikasi[index];
                 return Padding(
                   padding: EdgeInsets.only(bottom: 16),
-                  child: VerifikasiCard(
-                    nama: dosen.employee.employeeName,
-                    label: dosen.fieldName,
-                    status: dosen.status,
-                    value: dosen.newValue!,
+                  child: InkWell(
+                    onTap: () => {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => DetailVerifikasiScreen(
+                            verifikasiEntity: verifikasi,
+                          ),
+                        ),
+                      ),
+                    },
+                    child: VerifikasiCard(
+                      nama: verifikasi.employee.employeeName,
+                      label: verifikasi.fieldName,
+                      status: verifikasi.status,
+                      value: verifikasi.newValue!,
+                    ),
                   ),
                 );
               }, childCount: state.dataVerifikasi.length),
